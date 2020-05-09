50f5471cc6698bf6f5362ea886212252d736dcc7
Xing Shi Cai <newptcai@gmail.com>
2020-05-09 19:05:08 +0200

    Files generated by PkgTemplates

---
+++ src/PlaneGeomety.jl
@@ -0,0 +1,5 @@
+module PlaneGeomety
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PlaneGeomety
+using Test
+
+@testset "PlaneGeomety.jl" begin
+    # Write your own tests here.
+end
